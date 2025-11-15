02033da5a50e4e8c6ac937c11496b9bdb28d00d7
Quang <quang.anhp317@gmail.com>
2025-11-15 15:04:28 -0800

    setting up for neural network model

---
+++ src/neural_network.py
@@ -0,0 +1,7 @@
+"""
+Neural network Model for Diabetes Prediction using scikit-learn
+"""
+
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
